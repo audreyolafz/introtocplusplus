71b77d69a242c49a35b3987496483a194a4ba4b0
Audrey Wang <62197228+audreyolafz@users.noreply.github.com>
2023-09-15 21:10:45 -0700

    Upload chapter 1 lab

---
+++ Chapter 1/ch1_program_hw.cpp
@@ -0,0 +1,21 @@
+// Audrey Wang
+// 8/21/2023
+// Program P1.10
+// This program draws a house using print statements.
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "    /\\   " << endl;
+    cout << "   /  \\  " << endl;
+    cout << "  +----+  " << endl;
+    cout << "  | .-.|  " << endl;
+    cout << "  | | ||  " << endl;
+    cout << "  +-+-++  " << endl;
+    // return 0;
+}
